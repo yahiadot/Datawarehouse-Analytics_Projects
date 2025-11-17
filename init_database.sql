efc3e1d982ce474b39799c784c800165c11b4fbb
Yahia Bassam <77934337+yahiadot@users.noreply.github.com>
2025-11-17 15:38:12 +0200

    Create init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,11 @@
+===========================================================
+Create Database and Schemas
+==========================================================
+Script Purpose:
+This creates a new Database called 'Datawarehouse' after checking if is already exists. 
+if the database exists, its dropped and recreated. Additionaly the script sets up three schemas within the 
+database: (Silver Layer,Bronze Layer and gold).
+
+Warning: 
+Running the script will drop the entire database if it exists.
+All the data in the database will be permanently deleted.
